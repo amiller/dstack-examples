09d8b5d78ac9a57a059b71a1f64e04d22bb23ef7
Kevin Wang <wy721@qq.com>
2025-06-15 03:54:05 +0000

    Better package pinning

--- custom-domain/dstack-ingress/build-image.sh
+++ custom-domain/dstack-ingress/build-image.sh
@@ -8,4 +8,8 @@ fi
 if ! docker buildx inspect buildkit_20 &>/dev/null; then
     docker buildx create --use --driver-opt image=moby/buildkit:v0.20.2 --name buildkit_20
 fi
+touch pinned-packages.txt
+git rev-parse HEAD > .GIT_REV
 docker buildx build --builder buildkit_20 --no-cache --build-arg SOURCE_DATE_EPOCH="0" --output type=docker,name=$NAME,rewrite-timestamp=true .
+docker run --rm --entrypoint bash $NAME -c "dpkg -l | grep '^ii' |awk '{print \$2\"=\"\$3}' | sort" > pinned-packages.txt
+rm .GIT_REV
